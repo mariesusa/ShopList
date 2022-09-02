051446b23a301285b7e729ab58ef90a7980bca71
mariesusa <mari.remmler@hotmail.com>
2022-09-02 20:04:29 +0300

    ShoppingList

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const buttonAdd = () => {
+    setData([...data, { key: text }]);
+    setText('');
+    console.log(([...data, { key: text }]))
+  }
+
+  const buttonClear = () => {
+    setData([]);
+    console.log(([...data, { key: text }]))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <TextInput
+        style={ styles.input }
+        onChangeText={ text => setText(text) }
+        value={ text }
+      />
+    
+    <View style={{ flexDirection: 'row' }}>
+    <View style={ styles.button }>
+      <Button
+        onPress={ buttonAdd }
+        title='ADD'
+      />
+    </View>
+
+    <View style={ styles.button }>
+      <Button
+        onPress={ buttonClear }
+        title='CLEAR'
+      />
+    </View>
+    </View>
+
+    <Text
+      style={ styles.text }>
+      Shopping list
+    </Text>
+
+    <FlatList
+      data={ data }
+      renderItem={({ item }) => <Text>{ item.key }</Text>}
+      keyExtractor={( item, index ) => index.toString() }
+    />
+
     </View>
   );
 }
@@ -17,4 +65,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    width: 100,
+    height: 30,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 3,
+    marginTop: 100,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  button : {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'lightblue',
+    margin: 4,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '20%'
+  },
+  text : {
+    color: 'blue',
+    fontSize: 20,
+    marginBottom: 4,
+  }
 });
